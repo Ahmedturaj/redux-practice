6f5ce64b2153a25433c5644ebc4a929d100199b5
Ahmedturaj <ahmedturaj12@gmail.com>
2025-09-10 01:37:00 +0600

    added dom operation

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const handelIncrement =()=>{
 const decrement =()=>{
     count--;
 }
+
+document.getElementById('increment').addEventListener('click',handelIncrement);
+document.getElementById('decrement').addEventListener('click',decrement);
